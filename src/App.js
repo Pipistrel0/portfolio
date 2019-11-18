dbacc093d5227b81736875a09c7d268ab98a406e
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2019-11-18 01:48:09 -0300

    adding config of particles-js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Particles from "react-particles-js";
 import "./App.css";
+import { particlesConfig } from "./constants/particlesConfig";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Particles params={particlesConfig} />
+    </div>
+  );
 }
 
 export default App;
