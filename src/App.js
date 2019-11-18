b87a7262adfcf2731a3f2c8c264d8a23206990b1
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2019-11-18 12:44:41 -0300

    adding components and creating component Profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Particles from "react-particles-js";
+import Profile from "./components/Profile";
 import "./App.css";
 import { particlesConfig } from "./constants/particlesConfig";
 
 function App() {
   return (
     <div className="App">
-      <Particles params={particlesConfig} />
+      <Particles params={particlesConfig} className="particles" />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from "./node_modules/react";
+import Typical from "./node_modules/react-typical";
+
+const Profile = () => {
+  return (
+    <div>
+      <Typica />
+    </div>
+  );
+};
+
+export default Profile;
