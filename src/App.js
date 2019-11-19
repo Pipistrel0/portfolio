6bf5d405e0b8f91ad01b437932edf37211b6ca7f
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2019-11-19 19:31:48 -0300

    commit error

--- src/App.js
+++ src/App.js
@@ -8,19 +8,17 @@ import { particlesConfig } from "./constants/particlesConfig";
 
 function App() {
   return (
-    <div className="container">
-      <div className="app">
-        <div className="containerProfile">
-          <Profile />
-        </div>
-        <div className="containerButton">
-          <Button />
-        </div>
-        <div className="containerContact">
-          <Contact />
-        </div>
-        <Particles params={particlesConfig} className="particles" />
+    <div className="app">
+      <div className="containerProfile">
+        <Profile />
       </div>
+      <div className="containerButton">
+        <Button />
+      </div>
+      <div className="containerContact">
+        <Contact />
+      </div>
+      <Particles params={particlesConfig} className="particles" />
     </div>
   );
 }
