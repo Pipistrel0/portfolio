6ac17c469d7ae07ae40dbdfe7b2dfb86da35e6b0
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2019-11-25 01:31:56 -0300

    adding portofolio styles

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,7 +6,7 @@ const Portfolio = () => {
     <main className="container">
       <h2 className="header">
         Portafolio
-        <sub className="sub">asd</sub>
+        <sub className="sub">estos son los portafolios en los que trabaje</sub>
         <div className="portfolio">
           <div className="row">
             <div className="col-1-of-2 project">
