8ad010838a01d55f2fe2c2cd8f855f526763b41b
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2020-01-17 17:25:05 -0300

    Updates

---
+++ precache-manifest.9fd739af502e861a06f832daf918a375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a430395ef26f6e9b2dba84e0872f3dcd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "909e807938bda889e90a",
+    "url": "/portfolio/static/css/main.e4cc3d80.chunk.css"
+  },
+  {
+    "revision": "9d90077a262d79945a26",
+    "url": "/portfolio/static/js/2.0a214711.chunk.js"
+  },
+  {
+    "revision": "909e807938bda889e90a",
+    "url": "/portfolio/static/js/main.64280c1d.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "6199977c5d2c0fa282c9e5461acda434",
+    "url": "/portfolio/static/media/1.6199977c.jpg"
+  },
+  {
+    "revision": "37392a6f7788dbda0b2494e2270c9cfe",
+    "url": "/portfolio/static/media/2.37392a6f.jpg"
+  },
+  {
+    "revision": "db11287f69ad579576232b7d5373c7fd",
+    "url": "/portfolio/static/media/3.db11287f.jpg"
+  }
+]);
