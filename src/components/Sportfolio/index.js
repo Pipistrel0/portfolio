da7be3d8d80739cbf06fdac3b24b9c77baf8f613
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2020-01-20 20:46:54 -0300

    modals working

--- src/components/Sportfolio/index.js
+++ src/components/Sportfolio/index.js
@@ -17,10 +17,26 @@ class Sportfolio extends React.Component {
   }
   handleModal() {
     this.setState({ show: !this.state.show });
+  }
+
+  handleModalC() {
+    this.setState({ show: !this.state.show });
     this.setState({
-      project: document.getElementById("img").getAttribute("src")
+      project: document.getElementById("imgC").getAttribute("src")
     });
-    console.log(document.getElementById("img").getAttribute("src"));
+    console.log(this.state.show);
+  }
+  handleModalU() {
+    this.setState({ show: !this.state.show });
+    this.setState({
+      project: document.getElementById("imgU").getAttribute("src")
+    });
+  }
+  handleModalP() {
+    this.setState({ show: !this.state.show });
+    this.setState({
+      project: document.getElementById("imgP").getAttribute("src")
+    });
   }
 
   render() {
@@ -37,11 +53,10 @@ class Sportfolio extends React.Component {
             <figcaption>
               <h3>Gestor de Pasantias Web con Laravel</h3>
               <div className="additional">
-                <img id="img" src={utnl} alt="utn logo"></img>
-                {this.state.project === utnl ? null : null}
+                <img id="imgU" src={utnl} alt="utn logo"></img>
                 <Button
                   onClick={() => {
-                    this.handleModal();
+                    this.handleModalU();
                   }}
                   className="open-modal"
                 >
@@ -53,64 +68,207 @@ class Sportfolio extends React.Component {
                   ver proyecto>
                 </Button>
                 <Modal show={this.state.show}>
-                  <Modal.Body className="modal">
-                    <div className="project-modal">
-                      <div className="project-right">
-                        <div className="project-label">PROYECTO</div>
-                        <h3 className="project-name">GESTOR DE PASANTIAS</h3>
-                        <ul className="project-technologies">
-                          <li className="project-technology">HTML &amp; CSS</li>
-                          <li className="project-technology">JAVASCRIPT</li>
-                          <li className="project-technology">BOOTSTRAP</li>
-                          <li className="project-technology">PHP</li>
-                          <li className="project-technology">LARAVEL</li>
-                          <li className="project-technology">MYSQL</li>
-                        </ul>
-                        <div className="project-label">ACERCA DEL PROYECTO</div>
-                        <p className="project-summary">
-                          Gestor de pasantiasn en la utn
-                        </p>
-                        <div className="buttons">
-                          <div className="button-demo">
-                            <a
-                              href="https://www.pipistrel0.com"
-                              target="_blank"
-                              className="project-button project-button--modal"
-                              rel="noopener noreferrer"
-                            >
-                              <div className="button-svg">
-                                <FaEye size="1em" />
+                  <Modal.Body>
+                    {this.state.project === utnl ? (
+                      <div className="modal">
+                        <div className="project-modal">
+                          <div className="project-right">
+                            <div className="project-label">PROYECTO</div>
+                            <h3 className="project-name">
+                              GESTOR DE PASANTIAS
+                            </h3>
+                            <ul className="project-technologies">
+                              <li className="project-technology">
+                                HTML &amp; CSS
+                              </li>
+                              <li className="project-technology">JAVASCRIPT</li>
+                              <li className="project-technology">BOOTSTRAP</li>
+                              <li className="project-technology">PHP</li>
+                              <li className="project-technology">LARAVEL</li>
+                              <li className="project-technology">MYSQL</li>
+                            </ul>
+                            <div className="project-label">
+                              ACERCA DEL PROYECTO
+                            </div>
+                            <p className="project-summary">
+                              Gestor de pasantiasn en la utn
+                            </p>
+                            <div className="buttons">
+                              <div className="button-demo">
+                                <a
+                                  href="https://www.pipistrel0.com"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  DEMO
+                                </a>
                               </div>
-                              DEMO
-                            </a>
-                          </div>
-                          <div className="button-code">
-                            <a
-                              href="https://github.com/Pipistrel0"
-                              target="_blank"
-                              className="project-button project-button--modal"
-                              rel="noopener noreferrer"
-                            >
-                              <div className="button-svg">
-                                <FaEyeSlash size="1em" />
+                              <div className="button-code">
+                                <a
+                                  href="https://github.com/Pipistrel0"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaEyeSlash size="1em" />
+                                  </div>
+                                  CODIGO
+                                </a>
+                                <Button
+                                  className="close-modal"
+                                  onClick={() => {
+                                    this.handleModalC();
+                                  }}
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  Ver mas proyectos>
+                                </Button>
                               </div>
-                              CODIGO
-                            </a>
-                            <Button
-                              className="close-modal"
-                              onClick={() => {
-                                this.handleModal();
-                              }}
-                            >
-                              <div className="button-svg">
-                                <FaEye size="1em" />
-                              </div>
-                              Ver mas proyectos>
-                            </Button>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    ) : null}
+                    {this.state.project === cloudl ? (
+                      <div className="modal">
+                        <div className="project-modal">
+                          <div className="project-right">
+                            <div className="project-label">PROYECTO</div>
+                            <h3 className="project-name">WEATHER-APP</h3>
+                            <ul className="project-technologies">
+                              <li className="project-technology">
+                                HTML &amp; CSS
+                              </li>
+                              <li className="project-technology">JAVASCRIPT</li>
+                              <li className="project-technology">REACT.JS</li>
+                              <li className="project-technology">
+                                RESPONSIVE DESING
+                              </li>
+                              <li className="project-technology">API REST</li>
+                              <li className="project-technology">REDUX</li>
+                            </ul>
+                            <div className="project-label">
+                              ACERCA DEL PROYECTO
+                            </div>
+                            <p className="project-summary">
+                              Forecast Weather con react
+                            </p>
+                            <div className="buttons">
+                              <div className="button-demo">
+                                <a
+                                  href="https://www.pipistrel0.com"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  DEMO
+                                </a>
+                              </div>
+                              <div className="button-code">
+                                <a
+                                  href="https://github.com/Pipistrel0"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaCode />
+                                  </div>
+                                  CODIGO
+                                </a>
+                                <Button
+                                  className="close-modal"
+                                  onClick={() => {
+                                    this.handleModal();
+                                  }}
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  Ver mas proyectos>
+                                </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ) : null}
+                    {this.state.project === portfoliol ? (
+                      <div className="modal">
+                        <div className="project-modal">
+                          <div className="project-right">
+                            <div className="project-label">PROYECTO</div>
+                            <h3 className="project-name">PORTAFOLIO</h3>
+                            <ul className="project-technologies">
+                              <li className="project-technology">
+                                HTML &amp; CSS
+                              </li>
+                              <li className="project-technology">JAVASCRIPT</li>
+                              <li className="project-technology">REACT.JS</li>
+                              <li className="project-technology">
+                                RESPONSIVE DESING
+                              </li>
+                              <li className="project-technology">UX/UI</li>
+                            </ul>
+                            <div className="project-label">
+                              ACERCA DEL PROYECTO
+                            </div>
+                            <p className="project-summary">
+                              Portafolio con react
+                            </p>
+                            <div className="buttons">
+                              <div className="button-demo">
+                                <a
+                                  href="https://www.pipistrel0.com"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  DEMO
+                                </a>
+                              </div>
+                              <div className="button-code">
+                                <a
+                                  href="https://github.com/Pipistrel0"
+                                  target="_blank"
+                                  className="project-button project-button--modal"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="button-svg">
+                                    <FaCode />
+                                  </div>
+                                  CODIGO
+                                </a>
+                                <Button
+                                  className="close-modal"
+                                  onClick={() => {
+                                    this.handleModalC();
+                                  }}
+                                >
+                                  <div className="button-svg">
+                                    <FaEye size="1em" />
+                                  </div>
+                                  Ver mas proyectos>
+                                </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ) : null}
                   </Modal.Body>
                 </Modal>
               </div>
@@ -121,10 +279,10 @@ class Sportfolio extends React.Component {
             <figcaption>
               <h3>Pronostico del Clima con React</h3>
               <div className="additional">
-                <img src={cloudl} alt="cloud logo"></img>
+                <img id="imgC" src={cloudl} alt="cloud logo"></img>
                 <Button
                   onClick={() => {
-                    this.handleModal();
+                    this.handleModalC();
                   }}
                   className="open-modal"
                 >
@@ -135,67 +293,6 @@ class Sportfolio extends React.Component {
                   </IconContext.Provider>
                   ver proyecto>
                 </Button>
-                <Modal show={this.state.show}>
-                  <Modal.Body className="modal">
-                    <div className="project-right">
-                      <div className="project-label">PROYECTO</div>
-                      <h3 className="project-name">WEATHER-APP</h3>
-                      <ul className="project-technologies">
-                        <li className="project-technology">HTML &amp; CSS</li>
-                        <li className="project-technology">JAVASCRIPT</li>
-                        <li className="project-technology">REACT.JS</li>
-                        <li className="project-technology">
-                          RESPONSIVE DESING
-                        </li>
-                        <li className="project-technology">API REST</li>
-                        <li className="project-technology">REDUX</li>
-                      </ul>
-                      <div className="project-label">ACERCA DEL PROYECTO</div>
-                      <p className="project-summary">
-                        Forecast Weather con react
-                      </p>
-                      <div className="buttons">
-                        <div className="button-demo">
-                          <a
-                            href="https://www.pipistrel0.com"
-                            target="_blank"
-                            className="project-button project-button--modal"
-                            rel="noopener noreferrer"
-                          >
-                            <div className="button-svg">
-                              <FaEye size="1em" />
-                            </div>
-                            DEMO
-                          </a>
-                        </div>
-                        <div className="button-code">
-                          <a
-                            href="https://github.com/Pipistrel0"
-                            target="_blank"
-                            className="project-button project-button--modal"
-                            rel="noopener noreferrer"
-                          >
-                            <div className="button-svg">
-                              <FaCode />
-                            </div>
-                            CODIGO
-                          </a>
-                          <Button
-                            className="close-modal"
-                            onClick={() => {
-                              this.handleModal();
-                            }}
-                          >
-                            <div className="button-svg">
-                              <FaEye size="1em" />
-                            </div>
-                            Ver mas proyectos>
-                          </Button>
-                        </div>
-                      </div>
-                    </div>
-                  </Modal.Body>
-                </Modal>
               </div>
             </figcaption>
           </figure>
@@ -204,15 +301,20 @@ class Sportfolio extends React.Component {
             <figcaption>
               <h3>Portafolio Personal con React</h3>
               <div className="additional">
-                <img src={portfoliol} alt="portfolio logo"></img>
-                <button className="open-modal">
+                <img id="imgP" src={portfoliol} alt="portfolio logo"></img>
+                <Button
+                  onClick={() => {
+                    this.handleModalP();
+                  }}
+                  className="open-modal"
+                >
                   <IconContext.Provider
                     value={{ color: "#000", size: "1.3em" }}
                   >
                     <FaEye />
                   </IconContext.Provider>
                   ver proyecto>
-                </button>
+                </Button>
               </div>
             </figcaption>
           </figure>
